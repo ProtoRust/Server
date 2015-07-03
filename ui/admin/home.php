adb5fca1fc48da8550a255ff8c6189467dae53d2
VisionMise <geoffrey.kuhl@gmail.com>
2015-07-03 15:47:26 -0400

    Added Admin Home Page
    [#98338842][#98337284][#98342070]

---
+++ ui/admin/home.php
@@ -0,0 +1,21 @@
+<div class="hidden-xs panel-heading">
+	<h2>
+		Protorust Server
+		<br>
+		<small>A Web Request Framework for OxideMod</small>
+	</h2>
+</div>
+<div class="panel-body">
+	
+	<div class="container-fluid tight spaceless">
+		<div class="row-fluid tight spaceless">
+			<br>
+			<div class=" panel col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+				<h4 class="right">Welcome Admin</h4>
+				<div>
+					<div style="height:500px">&nbsp;</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ ui/admin/template.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>ProtoRust</title>
+
+		<!-- Startup configuration -->
+		<link rel="manifest" href="<?php print RELROOT;?>assets/web.manifest">
+
+		<!-- Fallback application metadata for legacy browsers -->
+		<meta name="application-name" content="ProtoRust">
+		<meta name="author" content="VisionMise">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" sizes="16x16 24x24 32x32 48x48 64x64 72x72 96x96 128x128 256x256" href="<?php print RELROOT;?>assets/img/ico/protorust.ico">
+
+		<!-- JQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+		<!-- Bootstrap 3.3.5 -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+		<!-- Bootstrap theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+		<!-- Protorust Style -->
+		<link rel="stylesheet" href="<?php print RELROOT;?>assets/css/main-style.css">
+
+		<!-- Protorust Script -->
+		<script src="<?php print RELROOT;?>assets/js/protorust.js"></script>
+	</head>
+	<body>
+		<div id="viewport">
+			<div class="hidden-xs" style="height:20%;">&nbsp;</div>
+			<div class="container-fluid spaceless tight">
+				<div class="row-fluid spaceless tight">
+					<div class="col-sm-8 col-sm-offset-2 tight">
+						<div class="panel panel-body rusted" id="page">
+							%page%
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
